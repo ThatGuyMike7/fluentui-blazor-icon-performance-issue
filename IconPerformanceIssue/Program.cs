6f597fa63d6a7e5725095594ba34da857f84c3d5
Mike Jasinski <mjasin@itc-chrysler.de>
2023-04-14 09:27:53 +0200

    Initial commit

---
+++ IconPerformanceIssue/Program.cs
@@ -0,0 +1,16 @@
+using IconPerformanceIssue;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents(
+    new LibraryConfiguration(
+        ConfigurationGenerator.GetIconConfiguration(),
+        ConfigurationGenerator.GetEmojiConfiguration()));
+
+await builder.Build().RunAsync();
